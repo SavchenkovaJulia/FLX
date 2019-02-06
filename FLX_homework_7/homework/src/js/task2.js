ae0d29dba57036ebec3fcb0e8ae2d8d569e346c9
SavchenkovaJulia <sawjulia9@gmail.com>
2019-02-06 23:57:14 +0200

    removed functions from task2

--- FLX_homework_7/homework/src/js/task2.js
+++ FLX_homework_7/homework/src/js/task2.js
@@ -8,37 +8,41 @@ let possiblePrize = stageMaxPrize;
 const start = confirm('Do you want to play a game?');
 
 if (start) {
-  startGame();
-} else {
-  alert('You did not become a millionaire, but can.');
-}
+  let currentAttempt;
+  while (currentAttempt !== 0) {
+    for (currentAttempt = attempts; currentAttempt > 0; currentAttempt--) {
+      let numberInput = parseFloat(
+        prompt(
+          `Enter a number from 0 to ${maxRange}
+          Attempts left: ${currentAttempt}
+          Total Prize: ${prize}
+          Possible prize on current attempt: ${possiblePrize} $`,
+          ''
+        )
+      );
+      if (randomNumber === numberInput) {
+        prize = possiblePrize;
+        if (
+          confirm(
+            `Congratulation! Your prize is: ${prize}$. Do you want to continue?`
+          )
+        ) {
+          stageMaxPrize = stageMaxPrize * 3;
+          possiblePrize = stageMaxPrize;
+          maxRange = maxRange * 2;
+          randomNumber = Math.floor(Math.random() * maxRange);
 
-function startGame() {
-  for (let currentAttempt = attempts; currentAttempt > 0; currentAttempt--) {
-    let numberInput = parseFloat(
-      prompt(
-        `Enter a number from 0 to ${maxRange}
-        Attempts left: ${currentAttempt}
-        Total Prize: ${prize}
-        Possible prize on current attempt: ${possiblePrize} $`,'')
-    );
-
-    if (randomNumber === numberInput) {
-      prize = possiblePrize;
-      if (confirm( `Congratulation! Your prize is: ${prize}$. Do you want to continue?`)) {
-        stageMaxPrize = stageMaxPrize * 3;
-        possiblePrize = stageMaxPrize;
-        maxRange = maxRange * 2;
-        randomNumber = Math.floor(Math.random() * maxRange);
-
-        return startGame();
+          break;
+        } else {
+          alert(`Thank you for a game. Your prize is: ${prize}$`);
+          break;
+        }
       } else {
-        alert(`Thank you for a game. Your prize is: ${prize}$`);
-        break;
+        possiblePrize = Math.floor(possiblePrize / 2);
       }
-    } else {
-      possiblePrize = Math.floor(possiblePrize / 2);
     }
   }
   alert(`Thank you for a game. Your prize is: ${prize}$`);
+} else {
+  alert('You did not become a millionaire, but can.');
 }
