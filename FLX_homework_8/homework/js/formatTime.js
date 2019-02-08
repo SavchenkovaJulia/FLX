76a3308ff9e296c9f4a344dbdc1b3d1fb22eabe3
SavchenkovaJulia <sawjulia9@gmail.com>
2019-02-08 14:08:53 +0200

    added number convertion

--- FLX_homework_8/homework/js/formatTime.js
+++ FLX_homework_8/homework/js/formatTime.js
@@ -1,5 +1,5 @@
 function formatTime(x) {
-  let num = Math.abs(x);
+  let num = Math.abs(parseInt(x));
   let days = Math.floor(num / 1440);
   let hours = Math.floor((num - days * 1400) / 60);
   let minutes = num - (hours * 60 + days * 1400);
