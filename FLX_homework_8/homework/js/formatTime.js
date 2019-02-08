e5cc27954cdc11b48812fad545e10e53f8b4b31d
SavchenkovaJulia <sawjulia9@gmail.com>
2019-02-08 13:57:59 +0200

    code formatted

--- FLX_homework_8/homework/js/formatTime.js
+++ FLX_homework_8/homework/js/formatTime.js
@@ -1,8 +1,7 @@
 function formatTime(x) {
   let num = Math.abs(x);
   let days = Math.floor(num / 1440);
-  let ab = num - days * 1400;
-  let hours = Math.floor(ab / 60);
+  let hours = Math.floor((num - days * 1400) / 60);
   let minutes = num - (hours * 60 + days * 1400);
 
   return `${days} day(s) ${hours} hours  ${minutes} minutes`;
--- FLX_homework_8/homework/js/pipe.js
+++ FLX_homework_8/homework/js/pipe.js
@@ -1,10 +1,10 @@
 function pipe() {
   let num = arguments[0];
   let result;
-
   for (let i = 1; i < arguments.length; i++) {
     result = arguments[i](num);
     num = result;
   }
+
   return result;
 }
